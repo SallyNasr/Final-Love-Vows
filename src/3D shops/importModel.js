47102c11d096bc1f473e736d41c37a00968337eb
SallyNasr <sally.nasr153@gmail.com>
2023-10-14 13:02:13 +0300

    .

---
+++ src/3D shops/importModel.js
@@ -0,0 +1,45 @@
+import { useEffect } from 'react';
+
+import * as THREE from 'three';
+
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+
+import SceneInit from './lib/SceneInit';
+
+function DressThreeD() {
+    useEffect(() => {
+        const test = new SceneInit('myThreeJsCanvas');
+        test.initialize();
+        test.animate();
+
+        let loadedModel;
+        const glftLoader = new GLTFLoader();
+        glftLoader.load('/bride3dFashion/scene.gltf', (gltfScene) => {
+            loadedModel = gltfScene;
+            console.log(loadedModel);
+
+            gltfScene.scene.rotation.y = Math.PI / 8;
+            gltfScene.scene.position.y = 3;
+            gltfScene.scene.scale.set(10, 10, 10);
+            test.scene.add(gltfScene.scene);
+        });
+
+        const animate = () => {
+            if (loadedModel) {
+                loadedModel.scene.rotation.x += 0.01;
+                loadedModel.scene.rotation.y += 0.01;
+                loadedModel.scene.rotation.z += 0.01;
+            }
+            requestAnimationFrame(animate);
+        };
+        animate();
+    }, []);
+
+    return (
+        <div>
+            <canvas id="myThreeJsCanvas" />
+        </div>
+    );
+}
+
+export default DressThreeD;
--- src/pages/Ourownshop/ourshop.js
+++ src/pages/Ourownshop/ourshop.js
@@ -9,23 +9,77 @@ import ban2 from '../../images/bannner2.png'
 import FetchCarousel from "../uploadProducts/fetchImagesCarousel";
 import ImageUploadForm from "../uploadProducts/ImageUploadForm";
 import uploadMultiImages from '../uploadProducts/uploadMultiImages';
-import DressThreeD from "../../3D shops/assets/importModel";
+import DressThreeD from "../../3D shops/importModel";
+
 
 export const OurShop = () => {
   return (
     <>
-      {/* <Navigationbar />
+      <Navigationbar />
 
       <img src={ban2} alt='banner-image1' style={{ position: "relative" }} height='65px' width='100%' />
       <div className="Ourshop">
         <div className="ourshopTitle">
-          <h1>Welcome to our own Shop</h1> */}
+          <h1>Welcome to our own Shop</h1>
 
-<DressThreeD/>
+          <DressThreeD />
+          {/* DREES 1 */}
+          <div className="sketchfab-embed-wrapper">
+            <iframe
+              title="The Bride"
+              frameBorder="0"
+              allowFullScreen
+              mozallowfullscreen="true"
+              webkitallowfullscreen="true"
+              allow="autoplay; fullscreen; xr-spatial-tracking"
+              execution-while-out-of-viewport
+              execution-while-not-rendered
+              web-share
+              src="https://sketchfab.com/models/6f72346c99d44a8cbc59bbe6630d0b20/embed"
+            ></iframe>
+            <p style={{
+              fontSize: "13px",
+              fontWeight: "normal",
+              margin: "5px",
+              color: "#4A4A4A"
+            }}>
+            </p>
+          </div>
+          {/* DRESS 2 */}
+          <div className="sketchfab-embed-wrapper">
+            <iframe
+              title="Erica Bride Fullbody"
+              frameBorder="0"
+              allowFullScreen
+              mozallowfullscreen="true"
+              webkitallowfullscreen="true"
+              allow="autoplay; fullscreen; xr-spatial-tracking"
+              xr-spatial-tracking
+              execution-while-out-of-viewport
+              execution-while-not-rendered
+              web-share
+              src="https://sketchfab.com/models/04940b90131b4c458f85f86ace3de329/embed"
+            ></iframe>
+          </div>
+          {/* DRES   <div className="sketchfab-embed-wrapper">
+      <iframe
+        title="3DFascination Bride"
+        frameBorder="0"
+        allowFullScreen
+        mozallowfullscreen="true"
+        webkitallowfullscreen="true"
+        allow="autoplay; fullscreen; xr-spatial-tracking"
+        xr-spatial-tracking
+        execution-while-out-of-viewport
+        execution-while-not-rendered
+        web-share
+        src="https://sketchfab.com/models/fd982b81899144308e18e2a54e02f897/embed"
+      ></iframe>S 3 */}
 
-          {/* <uploadMultiImages />
+
+          <uploadMultiImages />
         </div>
-        <ImageUploadForm />
+        {/* <ImageUploadForm /> */}
         {uploadMultiImages}
         <FetchCarousel />
         <div className="ourproducts">
@@ -34,7 +88,7 @@ export const OurShop = () => {
           ))}
         </div>
       </div>
-      <Footer /> */}
+      <Footer />
     </>
   );
 };

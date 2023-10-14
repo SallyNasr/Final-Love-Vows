e7a62c6511fed6630fd15f1d02c9d8781e4a72f9
SallyNasr <sally.nasr153@gmail.com>
2023-10-14 16:30:44 +0300

    .

--- src/pages/Ourownshop/ourproduct.js
+++ src/pages/Ourownshop/ourproduct.js
@@ -1,11 +1,30 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { ShopContext } from "./shop-context";
+import { useNavigate } from "react-router-dom";
+import { auth } from "../../firebase";
 
 export const Product = (props) => {
   const { id, productName, price } = props.data;
   const { addToCart, cartItems } = useContext(ShopContext);
 
   const cartItemCount = cartItems[id];
+  const navigate = useNavigate();
+
+  const [userLoggedIn, setUserLoggedIn] = useState(false);
+  const handleAddToCart = () => {
+    const user = auth.currentUser;
+    if (user) {
+      setUserLoggedIn(true);
+      addToCart(id);
+    } else if(!userLoggedIn){
+      alert('Please log in first.');
+    navigate("/login");
+      setUserLoggedIn(false);
+    }
+  }
+
+  
+
 
   return (
     <div className="product">
@@ -16,7 +35,7 @@ export const Product = (props) => {
         </p>
         <p> ${price}</p>
       </div>
-      <button className="addToCartBttn" onClick={() => addToCart(id)}>
+      <button className="addToCartBttn" onClick={handleAddToCart}>
         Add To Cart {cartItemCount > 0 && <> ({cartItemCount})</>}
       </button>
     </div>
